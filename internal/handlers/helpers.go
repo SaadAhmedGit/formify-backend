f6a11ed31a1336341705edc4bd6cdeda5d79a0d7
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-16 09:31:51 +0500

    Add verification email helper

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 type JSONResponse map[string]interface{}
@@ -20,4 +23,19 @@ func RespondWithJSON(w http.ResponseWriter, code int, payload JSONResponse) {
 func createJWT(claims jwt.Claims, jwtSecret string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(jwtSecret))
+}
+
+func sendVerificationEmail(payload PreSignUpDTO, tokenString string) error {
+	from := mail.NewEmail(env.APP_NAME, env.EMAIL_FROM)
+	subject := fmt.Sprintf("Verify your email for %s", env.APP_NAME)
+	to := mail.NewEmail(payload.FullName, payload.Email)
+	plainTextContent := fmt.Sprintf("Please verify your email by clicking on the following link: %s/signup?token=%s", env.DEV_SERVER_URL, tokenString)
+	htmlContent := fmt.Sprintf("Please verify your email by clicking on the following link: %s/signup?token=%s", env.DEV_SERVER_URL, tokenString)
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(env.SENDGRID_API_KEY)
+	_, err := client.Send(message)
+	if err != nil {
+		return err
+	}
+	return nil
 }
