ebd24fbaf6aaa179ae11ade7f6706839f66c7be1
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-16 09:33:03 +0500

    Add creation queries

--- internal/models/form.go
+++ internal/models/form.go
@@ -1,13 +1,42 @@
 package models
 
-type Form struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Owner       int64  `json:"owner"`
-	Description string `json:"description"`
-	URL         string `json:"url"`
-	PictureURL  string `json:"picture_url"`
+import (
+	"time"
 
-	CreatedAt int64 `json:"created_at"`
-	UpdatedAt int64 `json:"updated_at"`
+	"github.com/jmoiron/sqlx"
+)
+
+type Form struct {
+	ID          int64  `db:"id"`
+	Title       string `db:"title"`
+	Owner       int64  `db:"owner"`
+	Description string `db:"description"`
+	URL         string `db:"url"`
+	PictureURL  string `db:"picture_url"`
+
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+const CREATE_FORMS_TABLE_QUERY = `
+	CREATE TABLE IF NOT EXISTS forms (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		owner INT,
+		description TEXT,
+		url TEXT,
+		picture_url TEXT,
+		created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	);
+`
+
+func CreateForm(db *sqlx.DB, form Form) error {
+	query := `
+		INSERT INTO forms (title, owner, description, url, picture_url)
+		VALUES (:title, :owner, :description, :url, :picture_url)
+	`
+
+	_, err := db.NamedExec(query, &form)
+	return err
 }
