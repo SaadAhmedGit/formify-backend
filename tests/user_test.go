02acca8d1d594e58876aa019045995882a948e89
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-23 18:14:27 +0500

    Refactor user tests and add user deletion test

--- tests/user_test.go
+++ tests/user_test.go
@@ -5,39 +5,48 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/SaadAhmedGit/forms/internal/models"
+	"github.com/SaadAhmedGit/formify/internal/models"
 )
 
-func findUser(t *testing.T) {
+func TestUserCreation(t *testing.T) {
 	user := models.User{
 		FullName:       "John Doe",
 		Email:          "johndoe@example.com",
 		HashedPassword: "password123",
 	}
 
-	err = models.CreateUser(db, user)
+	err := models.CreateUser(db, user)
 	assert.NoError(t, err)
+}
 
-	user, err = models.FindUser(db, user.Email)
+func TestFindingUser(t *testing.T) {
+	user, err := models.FindUser(db, "johndoe@example.com")
 	assert.NoError(t, err)
 	assert.Equal(t, "John Doe", user.FullName)
 	assert.True(t, models.UserAuthorized(user.HashedPassword, "password123"))
 }
 
-func userDoesNotExist(t *testing.T) {
+func TestQueryingNonExistentUser(t *testing.T) {
 	_, err := models.FindUser(db, "emailthatwillprobablynevergetused@idk.com")
 	assert.Error(t, err)
 }
 
-func TestUser(t *testing.T) {
+func TestUserDeletion(t *testing.T) {
+	err := models.DeleteUser(db, "johndoe@example.com")
+	assert.Nil(t, err)
+}
 
-	// Create user schema
+func TestMain(m *testing.M) {
+	createSchema()
+	defer deleteSchema()
+
+	m.Run()
+}
+
+func createSchema() {
 	db.MustExec(models.CREATE_USERS_TABLE_QUERY)
+}
 
-	findUser(t)
-	userDoesNotExist(t)
-
-	//Delete user schema
-	db.MustExec("DROP TABLE users")
-
+func deleteSchema() {
+	db.MustExec(`DROP TABLE users`)
 }
