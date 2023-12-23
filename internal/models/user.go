0d86f22200f4bd065e3df34e4b099bf3d8e5dfc3
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-24 04:03:29 +0500

    Update hashed_password data type to VARCHAR in CREATE_USERS_TABLE_QUERY

--- internal/models/user.go
+++ internal/models/user.go
@@ -24,7 +24,7 @@ const CREATE_USERS_TABLE_QUERY = `
 		id SERIAL PRIMARY KEY,
 		full_name TEXT,
 		email TEXT NOT NULL UNIQUE,
-		hashed_password CHAR(255) NOT NULL,
+		hashed_password VARCHAR(255) NOT NULL,
 		created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 		updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 	);
@@ -32,16 +32,15 @@ const CREATE_USERS_TABLE_QUERY = `
 
 func UserExists(db *sqlx.DB, email string) (bool, error) {
 	var userCount int64
-	query := `
+	qry := `
 		SELECT COUNT(*) FROM users WHERE email = $1
 	`
 
-	db.Get(&userCount, query, email)
+	db.Get(&userCount, qry, email)
 	return userCount > 0, nil
 }
 
 func CreateUser(db *sqlx.DB, newUser User) error {
-
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.HashedPassword), bcrypt.DefaultCost)
 	if err != nil {
 		log.Printf("Failed to hash password: %v", err)
@@ -52,32 +51,30 @@ func CreateUser(db *sqlx.DB, newUser User) error {
 		HashedPassword: string(hashedPassword),
 	}
 
-	creationQuery := `
+	creationQry := `
 		INSERT INTO users (full_name, email, hashed_password)
 		VALUES (:full_name, :email, :hashed_password)
 	`
-	db.NamedExec(creationQuery, &user)
+	_, err = db.NamedExec(creationQry, &user)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func FindUser(db *sqlx.DB, email string) (User, error) {
 	var user User
-	query := `
+	qry := `
 		SELECT * FROM users
 		WHERE email=$1
 	`
 
-	row := db.QueryRowx(query, email)
-	if row == nil {
-		log.Printf("Failed to find user: %v", row)
-		return User{}, nil
-	}
-	err := row.StructScan(&user)
+	err := db.Get(&user, qry, email)
 	if err != nil {
-		log.Printf("Failed to scan user: %v", err)
 		return User{}, err
 	}
+
 	return user, nil
 }
 
@@ -93,11 +90,11 @@ func DeleteUser(db *sqlx.DB, email string) error {
 		}
 	}()
 
-	deleteQuery := `
+	deleteQry := `
 		DELETE FROM users
 		WHERE email = $1
 	`
-	result, err := tx.Exec(deleteQuery, email)
+	result, err := tx.Exec(deleteQry, email)
 	if err != nil {
 		return fmt.Errorf("failed to delete user with email %s: %v", email, err)
 	}
