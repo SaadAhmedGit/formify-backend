8114b84b38748b02a6f26fdcb38e77f2e2992e68
Saad Ahmed <saadahmedcronos@gmail.com>
2023-12-26 03:18:20 +0500

    Add CORS and http logging

--- cmd/formify/main.go
+++ cmd/formify/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/SaadAhmedGit/formify/internal/config"
 	"github.com/SaadAhmedGit/formify/internal/routes"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
@@ -18,9 +20,19 @@ func main() {
 
 	router := routes.NewRouter()
 
+	// CORS
+	headersOK := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOK := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	credentialsOK := handlers.AllowCredentials()
+	methodsOK := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"})
+
+	corsRouter := handlers.CORS(headersOK, originsOK, credentialsOK, methodsOK)(router)
+	loggedRouter := handlers.LoggingHandler(os.Stderr, corsRouter)
+
 	fmt.Printf("Listening on port %d...\n", env.PORT)
 	addr := fmt.Sprintf(":%d", env.PORT)
-	err = http.ListenAndServe(addr, router)
+
+	err = http.ListenAndServe(addr, loggedRouter)
 	if err != nil {
 		log.Fatalf("Failed to listen on port %d", env.PORT)
 	}
